9b52300c219c4e68461bb1e543c12ec20e881d1d
Glebson <glebsonsalmeida@gmail.com>
2025-04-03 07:23:26 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,57 @@
+const weatherApiKey = "d25f83878404746f0483dfec3c2bd5fd";
+const pexelsApiKey = "lySbgcALWnqsghKYKkwLZW7s5kJ4u1A6EgznD3xogk9grduC1GSnP0Nk";
+
+function colocarDadosNaTela(dados) {
+    document.querySelector(".cidade").innerHTML = "Clima em " + dados.name;
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C";
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description;
+    document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%";
+    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`;
+
+    buscaImagemPexels(dados.weather[0].main);
+}
+
+async function buscaCidade(cidade) {
+    try {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${weatherApiKey}&lang=pt_br&units=metric`;
+        const resposta = await fetch(url);
+        const dados = await resposta.json();
+
+        colocarDadosNaTela(dados);
+    } catch (erro) {
+        console.log("Erro ao buscar cidade!", erro);
+    }
+}
+
+async function buscaImagemPexels(clima) {
+    try {
+        const url = `https://api.pexels.com/v1/search?query=${clima} weather&per_page=1`;
+        const resposta = await fetch(url, {
+            headers: { Authorization: pexelsApiKey },
+        });
+        const dados = await resposta.json();
+
+        if (dados.photos.length > 0) {
+            const imageUrl = dados.photos[0].src.large;
+            document.body.style.backgroundImage = `url(${imageUrl})`;
+            document.body.style.backgroundSize = "cover";
+            document.body.style.backgroundPosition = "center";
+        } else {
+            console.log("Nenhuma imagem encontrada para:", clima);
+        }
+    } catch (erro) {
+        console.log("Erro ao buscar imagem no Pexels", erro);
+    }
+}
+
+function cliqueNoBotao() {
+    const cidade = document.querySelector(".input-cidade").value;
+    buscaCidade(cidade);
+}
+
+document.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        const cidade = document.querySelector(".input-cidade").value;
+        buscaCidade(cidade);
+    }
+});
